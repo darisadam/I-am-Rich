3ff935a63a377a4c5391b063c49dda7b131fd494
damdaris <darisadam@upi.edu>
2024-09-14 17:21:04 +0700

    Initial Commit

---
+++ I am Rich/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I am Rich
+//
+//  Created by Adam Daris Ryadhi on 14/09/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
